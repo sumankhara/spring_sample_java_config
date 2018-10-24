89d9a9d3ed4ceab6282e22b8022b0d589230cf1d
SK040459 <suman.khara@cerner.com>
2018-10-24 12:31:12 +0530

    first commit

---
+++ src/main/java/com/pluralsight/main/Application.java
@@ -0,0 +1,25 @@
+package com.pluralsight.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.pluralsight.service.CustomerService;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		
+		CustomerService service = appContext.getBean("customerService", CustomerService.class);
+		
+		System.out.println(service);
+		
+		CustomerService service2 = appContext.getBean("customerService", CustomerService.class);
+		
+		System.out.println(service2);
+		
+		System.out.println(service.findAll().get(0).getFirstname());
+	}
+
+}
